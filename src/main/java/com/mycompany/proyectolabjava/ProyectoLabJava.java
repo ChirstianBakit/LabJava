bcc51e9fec85b73272fb8ddc3afd3e123ec9c033
ChirstianBakit <Christianabakitr@gmail.com>
2024-11-28 22:22:09 -0400

    Java laboratorio

---
+++ src/main/java/com/mycompany/proyectolabjava/ProyectoLabJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectolabjava;
+
+/**
+ *
+ * @author chris
+ */
+public class ProyectoLabJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
