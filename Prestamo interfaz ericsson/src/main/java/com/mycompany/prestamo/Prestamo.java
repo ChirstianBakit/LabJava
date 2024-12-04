985f33e4a1d55b95d1c79c67752a0a5a978be488
Ericsson <ericssoncontreras386@gmail.com>
2024-12-04 01:22:21 -0400

    Subida de la interfaz gráfica del proyecto

---
+++ Prestamo interfaz ericsson/src/main/java/com/mycompany/prestamo/Prestamo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prestamo;
+
+/**
+ *
+ * @author user
+ */
+public class Prestamo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
